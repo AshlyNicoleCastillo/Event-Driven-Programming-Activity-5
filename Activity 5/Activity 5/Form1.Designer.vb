5d8dcd98eed5708f766d49fde4d9ccdba518998b
Ashly Nicole C. Castillo <130914508+AshlyNicoleCastillo@users.noreply.github.com>
2023-04-17 16:24:29 +0800

    Add files via upload

---
+++ Activity 5/Activity 5/Form1.Designer.vb
@@ -0,0 +1,175 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.DataGridView = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.BtnUpload = New System.Windows.Forms.Button()
+        Me.BtnNewFile = New System.Windows.Forms.Button()
+        Me.BtnDelete = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.BtnBackup = New System.Windows.Forms.Button()
+        Me.BtnView = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView
+        '
+        Me.DataGridView.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.DataGridView.Font = New System.Drawing.Font("Segoe UI", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.DataGridView.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.DataGridView.Location = New System.Drawing.Point(25, 25)
+        Me.DataGridView.Name = "DataGridView"
+        Me.DataGridView.Size = New System.Drawing.Size(529, 43)
+        Me.DataGridView.TabIndex = 1
+        Me.DataGridView.Text = "Data Grid View"
+        Me.DataGridView.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.BackgroundColor = System.Drawing.SystemColors.Window
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(25, 74)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 25
+        Me.DataGridView1.Size = New System.Drawing.Size(529, 346)
+        Me.DataGridView1.TabIndex = 2
+        '
+        'BtnUpload
+        '
+        Me.BtnUpload.BackColor = System.Drawing.Color.DodgerBlue
+        Me.BtnUpload.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.BtnUpload.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.BtnUpload.Location = New System.Drawing.Point(609, 125)
+        Me.BtnUpload.Name = "BtnUpload"
+        Me.BtnUpload.Size = New System.Drawing.Size(140, 38)
+        Me.BtnUpload.TabIndex = 3
+        Me.BtnUpload.Text = "UPLOAD"
+        Me.BtnUpload.UseVisualStyleBackColor = False
+        '
+        'BtnNewFile
+        '
+        Me.BtnNewFile.BackColor = System.Drawing.Color.PaleGreen
+        Me.BtnNewFile.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.BtnNewFile.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.BtnNewFile.Location = New System.Drawing.Point(609, 292)
+        Me.BtnNewFile.Name = "BtnNewFile"
+        Me.BtnNewFile.Size = New System.Drawing.Size(140, 38)
+        Me.BtnNewFile.TabIndex = 5
+        Me.BtnNewFile.Text = "NEW FILE"
+        Me.BtnNewFile.UseVisualStyleBackColor = False
+        '
+        'BtnDelete
+        '
+        Me.BtnDelete.BackColor = System.Drawing.Color.LightCoral
+        Me.BtnDelete.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.BtnDelete.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.BtnDelete.Location = New System.Drawing.Point(609, 352)
+        Me.BtnDelete.Name = "BtnDelete"
+        Me.BtnDelete.Size = New System.Drawing.Size(140, 38)
+        Me.BtnDelete.TabIndex = 6
+        Me.BtnDelete.Text = "DELETE"
+        Me.BtnDelete.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.Desktop
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.ForeColor = System.Drawing.SystemColors.Window
+        Me.Label1.Location = New System.Drawing.Point(609, 95)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(142, 17)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "By Ashly Nicole Castillo"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.SystemColors.Desktop
+        Me.Label2.Font = New System.Drawing.Font("Lucida Sans Unicode", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.ForeColor = System.Drawing.SystemColors.Window
+        Me.Label2.Location = New System.Drawing.Point(578, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(201, 34)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Data Loading"
+        '
+        'BtnBackup
+        '
+        Me.BtnBackup.BackColor = System.Drawing.Color.SandyBrown
+        Me.BtnBackup.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.BtnBackup.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.BtnBackup.Location = New System.Drawing.Point(609, 180)
+        Me.BtnBackup.Name = "BtnBackup"
+        Me.BtnBackup.Size = New System.Drawing.Size(140, 38)
+        Me.BtnBackup.TabIndex = 11
+        Me.BtnBackup.Text = "BACKUP"
+        Me.BtnBackup.UseVisualStyleBackColor = False
+        '
+        'BtnView
+        '
+        Me.BtnView.BackColor = System.Drawing.Color.Thistle
+        Me.BtnView.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.BtnView.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.BtnView.Location = New System.Drawing.Point(609, 236)
+        Me.BtnView.Name = "BtnView"
+        Me.BtnView.Size = New System.Drawing.Size(140, 38)
+        Me.BtnView.TabIndex = 12
+        Me.BtnView.Text = "VIEW"
+        Me.BtnView.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.BtnView)
+        Me.Controls.Add(Me.BtnBackup)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnDelete)
+        Me.Controls.Add(Me.BtnNewFile)
+        Me.Controls.Add(Me.BtnUpload)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.DataGridView)
+        Me.Name = "Form1"
+        Me.Text = "Data Loading , Conversion and Database Backup"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DataGridView As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents BtnUpload As Button
+    Friend WithEvents BtnNewFile As Button
+    Friend WithEvents BtnDelete As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents BtnBackup As Button
+    Friend WithEvents BtnView As Button
+End Class
---
+++ Activity 5/Activity 5/Form1.vb
@@ -0,0 +1,120 @@
+﻿Imports System.IO
+Imports System.Text
+
+Public Class Form1
+    Private Sub BtnUpload_Click(sender As Object, e As EventArgs) Handles BtnUpload.Click
+        Dim openFileDialog1 As New OpenFileDialog()
+        openFileDialog1.Filter = "CSV Files (*.csv)|*.csv"
+        openFileDialog1.Title = "Select a CSV File"
+        If openFileDialog1.ShowDialog() = DialogResult.OK Then
+            Dim csvData As String = File.ReadAllText(openFileDialog1.FileName)
+            Dim lines() As String = csvData.Split(ControlChars.Lf)
+            Dim data As New DataTable()
+            Dim headers() As String = lines(0).Split(",")
+            For Each header As String In headers
+                data.Columns.Add(header, GetType(String))
+            Next
+            For i As Integer = 1 To lines.Length - 1
+                Dim fields() As String = lines(i).Split(",")
+                data.Rows.Add(fields)
+            Next
+            DataGridView1.DataSource = data
+        End If
+    End Sub
+
+
+    Private Sub BtnBackup_Click(sender As Object, e As EventArgs) Handles BtnBackup.Click
+        Dim saveFileDialog1 As New SaveFileDialog()
+        saveFileDialog1.Filter = "CSV Files (*.csv)|*.csv"
+        saveFileDialog1.Title = "Save CSV File Backup"
+        If saveFileDialog1.ShowDialog() = DialogResult.OK Then
+            Dim writer As New StreamWriter(saveFileDialog1.FileName)
+            For Each row As DataGridViewRow In DataGridView1.Rows
+                Dim cells As List(Of String) = (From cell As DataGridViewCell In row.Cells Select Convert.ToString(cell.Value)).ToList()
+                writer.WriteLine(String.Join(",", cells))
+            Next
+            writer.Flush()
+            writer.Close()
+            MessageBox.Show("Backup successfully created!", "Backup Status", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+
+    Private Sub BtnView_Click(sender As Object, e As EventArgs) Handles BtnView.Click
+        Dim openFileDialog1 As New OpenFileDialog()
+        openFileDialog1.Filter = "CSV Files (*.csv)|*.csv"
+        openFileDialog1.Title = "Select a CSV File to View"
+        If openFileDialog1.ShowDialog() = DialogResult.OK Then
+            Dim reader As New StreamReader(openFileDialog1.FileName)
+            Dim csvData As New DataTable()
+            Dim isFirstRow As Boolean = True
+            While Not reader.EndOfStream
+                Dim line As String = reader.ReadLine()
+                If isFirstRow Then
+                    Dim headers As String() = line.Split(",")
+                    For Each header As String In headers
+                        csvData.Columns.Add(header.Trim())
+                    Next
+                    isFirstRow = False
+                Else
+                    Dim values As String() = line.Split(",")
+                    csvData.Rows.Add(values)
+                End If
+            End While
+            reader.Close()
+
+            Dim form As New Form()
+            form.Text = openFileDialog1.FileName
+            form.Width = 800
+            form.Height = 600
+
+            Dim dataGridView As New DataGridView()
+            dataGridView.DataSource = csvData
+            dataGridView.Dock = DockStyle.Fill
+
+            form.Controls.Add(dataGridView)
+            form.ShowDialog()
+        End If
+    End Sub
+
+
+    Private Sub BtnNewFile_Click(sender As Object, e As EventArgs) Handles BtnNewFile.Click
+        Dim data As New DataTable()
+        data.Columns.Add("Column1", GetType(String))
+        data.Columns.Add("Column2", GetType(String))
+        data.Columns.Add("Column3", GetType(String))
+        data.Columns.Add("Column4", GetType(String))
+        data.Columns.Add("Column5", GetType(String))
+        data.Rows.Add("Row1", GetType(String))
+        data.Rows.Add("Row2", GetType(String))
+        data.Rows.Add("Row3", GetType(String))
+        data.Rows.Add("Row4", GetType(String))
+        data.Rows.Add("Row5", GetType(String))
+        DataGridView1.DataSource = data
+
+        'Prompt the user to save the new file
+        Dim saveFileDialog1 As New SaveFileDialog()
+        saveFileDialog1.Filter = "CSV Files (*.csv)|*.csv"
+        saveFileDialog1.Title = "Save the new CSV file"
+        If saveFileDialog1.ShowDialog() = DialogResult.OK Then
+            'Save the data to the specified file
+            Dim sb As New StringBuilder()
+            Dim headers() As String = data.Columns.Cast(Of DataColumn)().Select(Function(c) c.ColumnName).ToArray()
+            sb.AppendLine(String.Join(",", headers))
+            For Each row As DataRow In data.Rows
+                Dim fields() As String = row.ItemArray.Select(Function(f) f.ToString()).ToArray()
+                sb.AppendLine(String.Join(",", fields))
+            Next
+            File.WriteAllText(saveFileDialog1.FileName, sb.ToString())
+        End If
+    End Sub
+
+
+    Private Sub BtnDelete_Click(sender As Object, e As EventArgs) Handles BtnDelete.Click
+        Dim result As DialogResult = MessageBox.Show("Are you sure you want to delete this file?", "Confirmation", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+            DataGridView1.DataSource = Nothing
+            MessageBox.Show("Deleted successfully!", "Backup Status", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+End Class
